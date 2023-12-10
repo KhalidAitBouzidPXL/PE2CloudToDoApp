2200f7412b0ce59ed6db0ef9a255ab0b25703615
khalid.aitbouzid@student.pxl.be <Khalid.aitBouzid@student.pxl.be>
2023-12-10 20:47:00 +0100

    Uploaded application code.

---
+++ backend/aws.js
@@ -0,0 +1,11 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: process.env.AWS_REGION});
+/*
+# These settings can be used if no propper IAM roles are set up or when running locally
+AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID});
+AWS.config.update({secretAccessKey: process.env.AWS_ACCESS_KEY});
+*/
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports = docClient;
---
+++ backend/routes/carrousel.routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const carrouselRouter = express.Router();
+
+const { S3Client, ListObjectsV2Command, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+
+const bucketName = 'pe2-khalidab-s3-bucket';
+const region = 'us-east-1';
+
+const listPublicFiles = async () => {
+  try {
+    const client = new S3Client({
+      region: region,
+    });
+    const command = new ListObjectsV2Command({ Bucket: bucketName });
+    const result = await client.send(command);
+    const publicUrls = await Promise.all(
+      result.Contents
+        .filter(obj => obj.Size > 0)
+        .map(async obj => {
+          const getObjectCommand = new GetObjectCommand({ Bucket: bucketName, Key: obj.Key });
+          const url = await getSignedUrl(client, getObjectCommand, { expiresIn: 3600 });
+          return { url: url };
+        })
+    );
+    console.log('publicUrls', publicUrls);
+    return publicUrls;
+  } catch (err) {
+    console.error('Error retrieving object URLs from S3 bucket:', err.message);
+    return [];
+  }
+};
+
+carrouselRouter.get('', async (req, res) => {
+  const publicUrls = await listPublicFiles();
+  res.json(publicUrls);
+});
+
+module.exports = carrouselRouter;
